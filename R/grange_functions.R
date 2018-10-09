072c65c4583a7ce16168d452938737a562c99f71
Enrique <enrique@MPHs-Air.fritz.box>
2018-10-09 17:46:25 +0200

    Added useful parser function

--- R/grange_functions.R
+++ R/grange_functions.R
@@ -60,7 +60,9 @@ pctOverlap = function(query, subject){
 #' @export
 #' 
 countsByChromosome <- function(gr, colName = 'Peptides') {
-       if(length(gr) != 0){
+         
+        if(length(gr) == 0) {stop('Input GRanges has length zero...')}
+
          # getting number of elements by chromosome as list...
          l <- lapply(split(gr, seqnames(gr)), function(x) length(x)) # split by chrom and get lenghts
          chrs <- names(l)
@@ -71,10 +73,9 @@ countsByChromosome <- function(gr, colName = 'Peptides') {
          names(df) <- c('Chromosome', colName) # rename df
          # sort by Chromosome
              df_ordered <- orderByChromosome(df, colName = 'Chromosome', ref.chr = c(1:22, 'X', 'Y', 'M'))
-         return(df)
-       } else {
-         return(NULL) # print msg here!
-       }
+         # remove row index
+             row.names(df_ordered) <- NULL
+         return(df_ordered)
 }
 
 
@@ -131,7 +132,7 @@ computeCoverageByChromosome <- function(query, subject, colName) {
   
   # Are the sequences comparable?
   if (!identical(seq_qry, seq_sub)){
-    stop('Entries contain different chromosome sizes/names...')
+    # stop('Entries contain different chromosome sizes/names...')
   }
   
   # compute coverage by chromosome
@@ -147,6 +148,7 @@ computeCoverageByChromosome <- function(query, subject, colName) {
   names(df) <- c('Chromosome', colName) # rename df
   df$Chromosome <- gsub(pattern = 'chr', replacement = '', x = df$Chromosome, fixed = TRUE)
   df_ordered <- orderByChromosome(df, colName = 'Chromosome', ref.chr = c(1:22, 'X', 'Y', 'M'))
+  row.names(df_ordered) <- NULL # remove row index/names
   return(df_ordered)
 }
 
@@ -167,3 +169,103 @@ computeCoverageByChromosome <- function(query, subject, colName) {
 orderByChromosome <- function(df, colName, ref.chr = c(1:22, 'X','Y','M')){
   return(df[order(match(as.character(df[,colName]), ref.chr)),])
 }
+
+
+#### count overlapped features ####
+
+## Compute overlap frequency of query on sugject
+
+computeOverlapFrequencyOnSubject <- function(query, subject, sizePopulation = NULL, overlap = 0.50, normalize = TRUE, cCol = 'counts_overlap', fCol = 'frequency' ) {
+  
+  #add column of counts. Initially it will be zero...
+  mcols(query)[,cCol] <- 0
+  mcols(query)[,fCol] <- 0
+  
+  # Find self hits
+  hits <- findOverlaps(query, subject, type = 'any', select = 'all', ignore.strand = TRUE)
+  
+  # If there aren't overlap between features, return the original set
+  if(length(hits)==0){
+    message('There is not overlapping features...')
+    return(query)
+  }
+  # Keep the hits that achieve the (or passing) pre-defined percent overlap:
+  
+  x <- query[queryHits(hits)]
+  y <- subject[subjectHits(hits)]
+  relative_overlap <- width(pintersect(x, y)) / pmin(width(x), width(y)) # relative overlap to width union query-subject
+  #relative_overlap <- width(pintersect(x, y)) / width(x) # relative overlap to width query
+  hits_set <- hits[relative_overlap >= overlap] #keep hits with at least x% overlap...
+  
+  # If there are not overlapping up defined threshold, return original set
+  if(length(hits_set)==0){
+    message('There is not overlapping features at this cutoff...')
+    return(query)
+  }
+  
+  #compute frecuency table
+  hits_frecuencies <- as.data.frame(table(queryHits(hits_set)))
+  names(hits_frecuencies) <- c("index_query", "counts_overlap")
+  
+  #append frecuencies to the original subset
+  granges_overlapping <- query[as.integer(hits_frecuencies$index_query)]
+  mcols(granges_overlapping)[,cCol] <- hits_frecuencies$counts_overlap
+  
+  #retrive non-overlapping regions fron original subset
+  granges_non_overlapping <- query[-as.integer(hits_frecuencies$index_query)]
+  
+  #build original GRanges object with frequencies
+  merged <- c(granges_overlapping, granges_non_overlapping)
+  
+  if (!is.null(sizePopulation) & normalize == TRUE ) {
+    mcols(merged)[,fCol] <- (mcols(merged)[,cCol]/sizePopulation)
+    mcols(merged)[,fCol] <- mcols(merged)[,fCol]/max(mcols(merged)[,fCol]) # normalize based on max F value
+    return(merged)
+  } else if (!is.null(sizePopulation) & normalize == FALSE ) {
+    mcols(merged)[,fCol] <- mcols(merged)[,cCol]/sizePopulation
+    return(merged)
+  } else {
+    mcols(merged)[,fCol] <- NULL
+    return(merged) 
+  }
+}
+
+
+
+## Compute simple overlap counts of query on sugject
+getOverlapCountOnSubject <- function(query, subject, cCol = 'counts'){
+  hits <- findOverlaps(query, subject, type = "any", select = "all", ignore.strand = TRUE)
+  q <- queryHits(hits)
+  freqTable <- as.data.frame(table(q), stringsAsFactors = FALSE) # overlap frequency of query on subject (any overlap)
+  overlapping_index <- as.integer(freqTable$q) # retriving index of overlapping features
+  query_overlapping <- query[overlapping_index] # getting overlapping features from original query set
+  query_non_overlapping <- query[-overlapping_index] # getting non-overlapping features from original query set
+  mcols(query_overlapping)[,cCol] <- freqTable$Freq # adding 'number of overlaps on subject' for each query overlapping instance
+  mcols(query_non_overlapping)[,cCol] <- 0 # adding 'zero' for each query non-overlapping instance
+  query_merged <- c(query_overlapping, query_non_overlapping) # merging
+  return(query_merged) # return original query with extra 'counts' column
+}
+
+## Convert Granges to DataFrame
+granges2dataframe <- function(grange, keep.metacols = TRUE){
+  # valid GRanges?
+  if(class(grange) != 'GRanges' | length(grange) == 0){
+    stop('Invalid input GRanges object...')
+  }
+     chrs <- as.character(GenomicRanges::seqnames(grange)) # getting chromosome info
+     starts <- as.integer(GenomicRanges::start(grange)) # getting starts position
+     ends <- as.integer(GenomicRanges::end(grange)) # getting ends position
+     
+     # crate basic df
+     df <- data.frame(chr=chrs, start=starts, end=ends)
+       
+     # checking for meta columns
+       if(keep.metacols){
+          metacols <- as.data.frame(mcols(grange))
+            if(ncol(metacols) != 0){
+              df <- cbind(df, metacols) # append meta info
+            }
+       }
+  return(df)
+}
+
